7a8c73efcff27b04650cc51005da662e6aa5f254
Kanatsanan Janpakdee <76254274+kanatsanan6@users.noreply.github.com>
2022-07-18 08:56:07 +0700

    day of week (#1)
    * day of week
    
    * change ruby version to 2.7.6
    
    * define days as class constant
    
    * change platform to ruby
    
    * define class instance once in the beginning
    
    * use described_class instead of using class name
    
    * Update DayOfWeek/lib/day_of_week.rb
    
    use %w shortcut to create an array of strings
    
    Co-authored-by: jsice <31514338+jsice@users.noreply.github.com>
    
    * Update DayOfWeek/lib/day_of_week.rb
    
    use index instead of find_index
    
    Co-authored-by: jsice <31514338+jsice@users.noreply.github.com>
    
    * restructuring file
    
    * adding more testcases
    
    * clean up Gemfile
    
    Co-authored-by: jsice <31514338+jsice@users.noreply.github.com>

---
+++ DayOfWeek/lib/day_of_week.rb
@@ -0,0 +1,10 @@
+class DayOfWeek
+  attr_accessor :day, :num
+
+  DAYS = %w[Mon Tue Wed Thu Fri Sat Sun].freeze
+
+  def solution(day, num)
+    day_index = DAYS.index(day)
+    DAYS[(day_index + num) % 7]
+  end
+end
---
+++ DayOfWeek/spec/day_of_week_spec.rb
@@ -0,0 +1,59 @@
+require 'day_of_week'
+
+describe DayOfWeek do
+  let(:day) { described_class.new }
+
+  describe 'solution' do
+    context 'sample testcases' do
+      it 'returns Fri' do
+        expect(day.solution('Wed', 2)).to eq 'Fri'
+      end
+
+      it 'returns Mon' do
+        expect(day.solution('Sat', 23)).to eq 'Mon'
+      end
+    end
+
+    context 'same week' dox
+      it 'returns Mon' do
+        expect(day.solution('Mon', 0)).to eq 'Mon'
+      end
+
+      it 'returns Wed' do
+        expect(day.solution('Tue', 1)).to eq 'Wed'
+      end
+
+      it 'returns Thu' do
+        expect(day.solution('Wed', 2)).to eq 'Fri'
+      end
+
+      it 'returns Sun' do
+        expect(day.solution('Thu', 3)).to eq 'Sun'
+      end
+    end
+
+    context 'next week' do
+      it 'returns Mon' do
+        expect(day.solution('Mon', 7)).to eq 'Mon'
+      end
+
+      it 'returns Wed' do
+        expect(day.solution('Tue', 8)).to eq 'Wed'
+      end
+
+      it 'returns Thu' do
+        expect(day.solution('Wed', 9)).to eq 'Fri'
+      end
+
+      it 'returns Sun' do
+        expect(day.solution('Thu', 10)).to eq 'Sun'
+      end
+    end
+
+    context 'big K' do
+      it 'returns Thu' do
+        expect(day.solution('Fri', 230)).to eq 'Thu'
+      end
+    end
+  end
+end
