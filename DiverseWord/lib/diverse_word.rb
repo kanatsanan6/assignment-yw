c5b486cccfdcbb168b91859c470eb8ff766a5113
kanatsanan6 <kanatsanan.j1998@gmail.com>
2022-07-18 10:02:48 +0700

    refactor DiverseWord

--- DiverseWord/lib/diverse_word.rb
+++ DiverseWord/lib/diverse_word.rb
@@ -4,14 +4,12 @@ class DiverseWord
   def solution(a, b, c)
     result = []
     remain_char = { 'a' => a, 'b' => b, 'c' => c }.sort_by { |_, value| -value }.to_h
-    (start, _), (stop, _) = remain_char.first 2
-    while remain_char[start].positive? and remain_char[stop].positive?
+    start, stop = remain_char.keys.first(2)
+    while remain_char[start].positive? && remain_char[stop].positive?
       result << start
       remain_char[start] -= 1
 
-      if result.length >= 2 and result[-2] == start
-        # do nothing
-      elsif remain_char[start] > 0
+      if remain_char[start] > 0 && (result.length < 2 or result[-2] != start)
         result << start
         remain_char[start] -= 1
       end
@@ -31,6 +29,6 @@ class DiverseWord
         result << start
       end
     end
-    result.join('')
+    result.join
   end
 end
